b4b17e9ffb643cfddce1c4c838b1d4d7f03b9ae7
Kai <52977355+NikaLost@users.noreply.github.com>
2021-06-14 18:31:54 +0200

    einfaches Spielfeld

--- Controller.java
+++ Controller.java
@@ -2,8 +2,14 @@ import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 public class Controller {
     private MeineFachklasse mFk = new MeineFachklasse();
+    
+    @FXML
+    GridPane spielfeld;
+    
+    
     
 }
