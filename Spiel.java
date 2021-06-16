854e4e67a77af7ecfcab38a34292e6e5ebd29a0f
Kai <52977355+NikaLost@users.noreply.github.com>
2021-06-16 12:59:48 +0200

    Basic Attribute und Methoden

--- Spiel.java
+++ Spiel.java
@@ -1,33 +1,51 @@
-
+import java.util.LinkedList;
 /**
- * Beschreiben Sie hier die Klasse Spiel.
+ * In dieser Klasse wird der Zustand des 4-gewinnt Spiels verwaltet.
+ * Zu einem 4-gewint Spiel gehört ein Spielbrett mit Spielsteinen und 2 Spieler.
  * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * @author Dominik F, Kai M, Kai S
+ * @version 16.06.2021
  */
 public class Spiel
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse Spiel
-     */
-    public Spiel()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
+    public static final int ANZ_SPIELER = 2;
+    public static final int STANDARD_ANZ_SPIELSTEINE = 21;
+    private Spieler[] spieler;
+    private Spielbrett spielbrett;
+    private int amZug;
+    private LinkedList<Farbe> farbenAuswahl;
+    
+    
+    Spiel() {
+        this.spieler = new Spieler[ANZ_SPIELER];
+        this.spielbrett = new Spielbrett();
+        amZug = 0;
+        for (Farbe farbe : Farbe.values()) {
+            farbenAuswahl.add(farbe);
+        }
     }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
+    
+    public void addSpieler(String name) {
+        for (int i = 0; i < spieler.length; i++) {
+            if (spieler[i] == null) {
+                spieler[i] = new Spieler(name, farbenAuswahl.remove(), STANDARD_ANZ_SPIELSTEINE);
+            }
+        }
     }
+    
+    public void einwerfen (int spalte) {
+        if (spieler[amZug].einwerfenIn(spielbrett, spalte)) {
+            naechsterSpieler();
+        }
+    }
+    
+    public Spieler amZug() {
+        return spieler[amZug];
+    }
+    
+    public void naechsterSpieler() {
+        amZug = (amZug + 1) % ANZ_SPIELER;
+    }
+    
+    
 }
